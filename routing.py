e88c94dbec2ac1873d6fbd6d9fda71abd94bca5f
Adrian-The-Techie <adrianmuthomie@gmail.com>
2024-07-30 11:21:55 +0300

    Updated routing url

--- routing.py
+++ routing.py
@@ -1,8 +1,7 @@
 from django.urls import path, re_path
-from django.conf.urls import url
 
 from payments import consumers
 
 websocket_urlpatterns = [
-    url(r"^ws/(?P<room_name>[^/]+)", consumers.ChatConsumer)
+    re_path(r"^ws/(?P<room_name>[^/]+)", consumers.ChatConsumer.as_asgi())
 ]
