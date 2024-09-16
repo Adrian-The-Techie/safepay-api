a1c823d79b24b3373ed71fb37b9cb8005e09db8e
CREATIVE BRANDS <creativebrands@CREATIVEs-MacBook-Pro.local>
2024-09-16 16:54:56 +0300

    fix:added GET capabilities

--- ussd/views.py
+++ ussd/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 import requests
 from decouple import config
+import json
 
 # Create your views here.
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 def index(request):
 
-    requests.post(config('USSD_TEST_ENDPOINT'), json=request.data)
+    requests.post(config('USSD_TEST_ENDPOINT'), data=request.data)
 
     return JsonResponse({"status": 1, "message": "USSD response"})
